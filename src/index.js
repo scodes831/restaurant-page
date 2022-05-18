1fb8e76dcbe578347f218b1060067275966e1018
scode234 <scodes3451@gmail.com>
2022-05-18 18:34:45 -0500

    Fix issue that prevented clicking on other page after clicking homepage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import { showHomepage } from './home.js';
 import { showAboutPage } from './about.js';
-import { Entree } from './menu.js';
+import { showContactPage } from './contact.js';
+import { showMenuPage } from './menu.js';
 
 import logoImage from './images/logo.png';
 console.log("It's working");
-
-import classicWafflesImage from './images/classic-waffles.jpg';
-import banNutWafflesImage from './images/banana-nut-waffles.jpg';
-import cherryCheesecakeWafflesImage from './images/cherry-cheesecake-waffles.jpg';
-import chocTurtleWafflesImage from './images/chocolate-turtle-waffles.jpg';
-import classicBerryWafflesImage from './images/classic-berry-waffles.jpg';
-import waffleSundaeImage from './images/waffle-sundae.jpg';
 
 const page = document.getElementById('content');
 
@@ -54,73 +48,79 @@ function showNavBar() {
     contactLink.setAttribute('id', 'contact');
     contactLink.textContent = "Contact";
     
-    let userClicks = ["homepage"];
+    let userClicks = ["home"];
     
     const pages = document.querySelectorAll('.nav-link');
     pages.forEach(page => {
         page.addEventListener('click', e => {
             if (page.classList.contains('active')) {
-                console.log("it's already active");
+                console.log("it's already active"); //if the page clicked is already active, do nothing
                 return;
             } else {
                 console.log(userClicks[userClicks.length-1]);
-                let currentPage = userClicks[userClicks.length-1];
-                if (currentPage === "homepage") {
-                    console.log("remove the homepage");
+                let currentPage = userClicks[userClicks.length-1]; //determines page we are currently on
+                if (currentPage === "home") { 
+                    console.log("remove the home page");
+                    homeLink.classList.remove('active');
+                    homeLink.classList.add('inactive');
+                    removeHomepage();
+
                 } else if (currentPage === "about" ) {
                     console.log("remove the about page");
-                    removePage();
-                    // currentPage.classList.remove('active');
-                    // currentPage.classList.add('inactive');
+                    aboutLink.classList.remove('active');
+                    aboutLink.classList.add('inactive');
+                    removeAboutPage();
+
                 } else if (currentPage === "menu") {
                     console.log("remove the menu page");
+                    menuLink.classList.remove('active');
+                    menuLink.classList.add('inactive');
+                    removeMenuPage();
                 } else {
                     console.log("remove the contact page");
+                    contactLink.classList.remove('active');
+                    contactLink.classList.add('inactive'); //based on current page, set it to inactive and remove page
+                    removeContactPage();
                 }
                 console.log("it's not active");
                 page.classList.remove('inactive');
                 page.classList.add('active');
-                userClicks.push(page.id);
+                userClicks.push(page.id); //set the clicked page to active and log click in array
                 if (page.id === "home") {
                     showHomepage();
                 } else if (page.id === "about") {
                     showAboutPage();
                 } else if (page.id === "menu") {
-                    console.log("this is the menu page");
+                    showMenuPage();
                 } else {
-                    console.log("this is the contact page");
+                    console.log("this is the contact page"); //load the correct page by page id
+                    showContactPage();
                 }
                 console.log(userClicks);
                
-
+                //can click multiple times except once click on homepage
+                //you can't click on any other page -- why?
             }
         })
     });
 
-    function removePage() {
-        const content = document.getElementById('outer-container');
+    function removeContactPage() {
+        const content = document.getElementById('contact-container');
+        content.remove();
+    };
+
+    function removeHomepage() {
+        const content = document.getElementById('content-container');
+        content.remove();
+    };
+
+    function removeAboutPage() {
+        const content = document.getElementById('outer-about-container');
+        content.remove();
+    };
+
+    function removeMenuPage() {
+        const content = document.getElementById('cards-container');
         content.remove();
     }
-        
-
-
-
-// function showMenuPage() {
-//     console.log("menu page clicked");
-//     // const entree1 = new Entree(classicBerryWafflesImage, "Classic Berry Waffles", 6.49, "Classic belgian waffles topped with fresh berries, granola, and whipped cream.");
-//     // entree1.addCard();
-//     // entree1.addImage();
-//     // entree1.addName();
-//     // entree1.addPrice();
-//     // entree1.addDescription();
-
-//     // const entree2 = new Entree(cherryCheesecakeWafflesImage, "Cherry Cheesecake Waffles", 7.49, "Classic belgian waffles topped with cheesecake filling, cherry topping, and whipped cream.");
-//     // const entree3 = new Entree(classicWafflesImage, "Classic Waffles", 4.99, "Classic begian waffles dusted with powdered sugar.");
-//     // const entree4 = new Entree(waffleSundaeImage, "Waffle Sundae", 3.99, "Classic belgian waffle with a scoop of homemade vanilla ice cream, topped with chocolate sauce, almonds, and whipped cream.");
-//     // const entree5 = new Entree(banNutWafflesImage, "Banana Nut Waffles", 7.49, "Classic belgian waffles topped with sliced bananas, cinnamon, walnuts, and powdered sugar.");
-//     // const entree6 = new Entree(chocTurtleWafflesImage, "Chocolate Turtle Waffles", 7.49, "Classic belgian waffles topped with chocolate sauce, caramel sauce, pecans, and whipped cream.")
-// };
-
-// function showContactPage() {
-//     console.log("contact page clicked");
-}
+};
